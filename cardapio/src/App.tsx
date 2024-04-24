e3b2194fedeb62a01c0c72a27bf9bffeeb9719a4
Thainá R Zanfolin <102308025+thainazanfolin@users.noreply.github.com>
2024-04-24 17:17:44 -0300

    start frontend react and typescript

---
+++ cardapio/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <h1>Nosso cardápio</h1>
+        <div className="card-grid">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ cardapio/src/components/cards/card.tsx
@@ -0,0 +1,15 @@
+interface CardProps {
+    price: number,
+    title: string,
+    image: string
+}
+
+export function Card({price, image, title} : CardProps){
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2></h2>
+            <p><b>R$</b></p>
+        </div>
+    )
+}
